fd1c819ecc9affa56f14c8e66aa34457b9e7d59e
E-Commerce <tushargahtori77@gmail.com>
2026-05-21 11:15:38 +0530

    feat: integrate Prometheus and Grafana observability stack
    - Added Spring Boot Actuator and Micrometer Prometheus registry.
    - Whitelisted /actuator/prometheus endpoint in Spring Security configuration.
    - Created prometheus.yml to scrape backend application metrics.
    - Added Prometheus and Grafana services to the master docker-compose.yml.
    - Configured JVM Micrometer dashboard for real-time traffic and performance monitoring.

--- src/main/java/com/example/SmartEcommercePlatform/Security/SecurityConfig.java
+++ src/main/java/com/example/SmartEcommercePlatform/Security/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/actuator/**"
                         ).permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()                 // login only
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/**").permitAll()// login only
                         .requestMatchers("/api/users/**").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/products/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/products/**").hasRole("ADMIN")
